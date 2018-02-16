873f9f683157ca4c5c30297dd7c44902e870fc77
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2018-02-16 09:05:12 +0100

    Basic example of incomming and outgoing data
    The simplest way to deal with server calls in Elm to start with is to
    use ports and javascript ajax calls. This is for example useful if you
    want to use the phoenix websocket client.

--- src/Main.elm
+++ src/Main.elm
@@ -1,19 +1,48 @@
-module Main exposing (..)
+port module Main exposing (..)
 
-import Html exposing (Html, text, div, h1, img)
-import Html.Attributes exposing (src)
+import Html exposing (Html, text, div, h1, img, button)
+import Html.Attributes exposing (src, class)
+import Html.Events exposing (onClick)
+
+
+---- PORTS ----
+
+
+port serverStateUpdate : (ServerState -> msg) -> Sub msg
+
+
+port outgoingCommand : String -> Cmd msg
+
 
 
 ---- MODEL ----
 
 
+type alias Job =
+    { id : String
+    , worker : String
+    }
+
+
+type alias ServerState =
+    { failedJobs : List Job
+    }
+
+
 type alias Model =
-    {}
+    { serverState : Maybe ServerState
+    , someLocalState : String
+    }
+
+
+initialModel : Model
+initialModel =
+    { serverState = Nothing, someLocalState = "Check your browser console when pressing \"Retry\"..." }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( {}, Cmd.none )
+    ( initialModel, Cmd.none )
 
 
 
@@ -22,11 +51,29 @@ init =
 
 type Msg
     = NoOp
+    | UpdateServerState ServerState
+    | RetryJob Job
+
+
+subscriptions model =
+    [ serverStateUpdate UpdateServerState
+    ]
+        |> Sub.batch
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+        UpdateServerState serverState ->
+            ( { model | serverState = Just serverState }, Cmd.none )
+
+        RetryJob job ->
+            ( { model | someLocalState = "Sent retry request" }
+            , outgoingCommand ("retryJob:" ++ job.id)
+            )
 
 
 
@@ -36,8 +83,29 @@ update msg model =
 view : Model -> Html Msg
 view model =
     div []
-        [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working!" ]
+        [ h1 [] [ text "Example" ]
+
+        -- This could be anything that we don't get from the server and only change locally like what view you are currently looking at.
+        , text model.someLocalState
+        , viewFailedJobs model
+        ]
+
+
+viewFailedJobs : Model -> Html Msg
+viewFailedJobs model =
+    case model.serverState of
+        Just serverState ->
+            div [] (serverState.failedJobs |> List.map viewFailedJob)
+
+        Nothing ->
+            text "Loading..."
+
+
+viewFailedJob job =
+    div [ class "job" ]
+        [ div [] [ text ("ID: " ++ job.id) ]
+        , div [] [ text ("Worker: " ++ job.worker) ]
+        , button [ onClick (RetryJob job) ] [ text "Retry" ]
         ]
 
 
@@ -51,5 +119,5 @@ main =
         { view = view
         , init = init
         , update = update
-        , subscriptions = always Sub.none
+        , subscriptions = subscriptions
         }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,17 @@ import './main.css';
 import { Main } from './Main.elm';
 import registerServiceWorker from './registerServiceWorker';
 
-Main.embed(document.getElementById('root'));
+let elm = Main.embed(document.getElementById('root'));
+
+elm.ports.outgoingCommand.subscribe(function(command) {
+    console.log({ outgoingCommand: command })
+})
+
+elm.ports.serverStateUpdate.send({
+    failedJobs: [
+        { id: "test", worker: "FooWorker" },
+        { id: "test2", worker: "FooWorker" }
+    ]
+})
 
 registerServiceWorker();
